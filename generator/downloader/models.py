181611e1b621951b03661148a09d8f71355ec88d
atishayjain2003 <atishayjain2003@gmail.com>
2024-08-19 08:01:21 +0530

    Adding model

--- generator/downloader/models.py
+++ generator/downloader/models.py
@@ -1,3 +1,41 @@
 from django.db import models
 
-# Create your models here.
+class Resume(models.Model):
+    # Basic Information
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    location = models.CharField(max_length=100)
+    email = models.EmailField()
+    linkedin = models.URLField()
+    website = models.URLField()
+
+    # Objective
+    objective = models.TextField()
+
+    # Education
+    education1 = models.TextField()
+    education2 = models.TextField()
+
+    # Skills
+    technical_skills = models.TextField()
+    soft_skills = models.TextField()
+    other_skills = models.TextField()
+
+    # Experience
+    experience1 = models.TextField()
+    experience2 = models.TextField()
+
+    # Projects
+    project1 = models.TextField()
+    project2 = models.TextField()
+    project3 = models.TextField()
+
+    # Extra-Curricular Activities
+    activities = models.TextField()
+
+    # Leadership
+    leadership = models.TextField()
+
+    def __str__(self):
+        return self.name
+
